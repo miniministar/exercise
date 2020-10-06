9a266870ea327cafaa6d933fa3ed1c4c36fe7e4c
ranqing <1005712028@qq.com>
2020-10-07 03:33:32 +0800

    获取蘑菇街推荐商品数据做展示

--- mall-server/src/main/java/com/exercise/controller/MoguController.java
+++ mall-server/src/main/java/com/exercise/controller/MoguController.java
@@ -77,9 +77,9 @@ public class MoguController {
     @ApiOperation(value = "", notes = " {\"page\":1,\"pageSize\":20,\"isGoBackup\":true,\"cKey\":\"app-clothing-v1\",\"fcid\":\"20000484\"}")
     public String getDetail(@RequestBody QueryVo vo) {
         UrlParams urlParams = new UrlParams();
-        urlParams.setCookie("__mgjuuid=3e482381-6a92-4853-a140-76c06279731a; FRMS_FINGERPRINTN=_51veHI3TjmsSvMSjwKUVw; _ga=GA1.2.1520040884.1600612476; _mwp_h5_token_enc=2051c5bde0e1e5aa6669ab30bd9216ea; _mwp_h5_token=b0dbd2998c04c90da0a092d7763bd2fc_1601300153679; _gid=GA1.2.766618084.1601300195");
-        urlParams.setReferer("https://m.mogu.com/wall/s?q=%E5%A5%B3%E8%A3%85&ptp=32._mf1_1239_4537.0.0.0gB0BHOW&acm=3.mce.1_10_1prjy.140653.0.gGQ8fsbk55526.pos_873-m_601027-sd_119-mf_4537_1211938-idx_1-mfs_15-dm1_5000");
-        String url = "https://api.mogu.com/h5/http.detail.api/1/?data=%7B%22appPlat%22%3A%22m%22%2C%22sourceParams%22%3A%22%22%2C%22template%22%3A%221-2-detail_normal-1.0.0%22%2C%22iid%22%3A%221mviwp6%22%2C%22activityId%22%3A%22%22%7D&mw-appkey=100028&mw-ttid=NMMain%40mgj_h5_1.0&mw-t=1601300733319&mw-uuid=3e482381-6a92-4853-a140-76c06279731a&mw-h5-os=iOS&mw-sign=124d6cb51cac1e3273f981f0a7f7f665&callback=mwpCb1&_=1601300733327";
+        urlParams.setCookie("__mgjuuid=3e482381-6a92-4853-a140-76c06279731a; FRMS_FINGERPRINTN=_51veHI3TjmsSvMSjwKUVw; _ga=GA1.2.1520040884.1600612476; _mwp_h5_token_enc=eda2deb3fa51cc248bb4b3f4fdad0b56; _mwp_h5_token=564d4fa3cfcfdc97917c04767732e6db_1601905095482");
+        urlParams.setReferer("https://h5.mogu.com/detail-normal/index.html?itemId=1mjtcfo&acm=3.ms.1_1_1mjtcfo.46.86084-68998.e6MRRscyazKEA.ct_1200-sd_117-swt_46-imt_7-c_2_17_588337913_511007437_1_0-t_e6MRRscyazKEA-lc_17-pit_2-c1_88933_nil_nil_1_511007437_45_0-dit_-idx_0-dm1_5001&cparam=MTYwMTkwNTEwNl8xam15b2o0X2Y5YmExNWE1MWM5NzYxNDhlZTg2N2MzMDE1ZGE5ZTgyXzE3XzBfNTg4MzM3OTEzXzQ5OGRfMF8xXzBfNjYzXzFfMF9ycy04ODkzMy5ydC0xLnd0LTE=&ptp=32.rPCjb.undefined.1.LEjckPM4");
+        String url = "https://api.mogu.com/h5/http.detail.api/1/?data=%7B%22appPlat%22%3A%22m%22%2C%22sourceParams%22%3A%22%22%2C%22template%22%3A%221-2-detail_normal-1.0.0%22%2C%22iid%22%3A%221mjtcfo%22%2C%22activityId%22%3A%22%22%7D&mw-appkey=100028&mw-ttid=NMMain%40mgj_h5_1.0&mw-t=1601905111160&mw-uuid=3e482381-6a92-4853-a140-76c06279731a&mw-h5-os=iOS&mw-sign=329f990240cde208a506b6b94ec530e6&callback=mwpCb1&_=1601905111163";
         urlParams.setUrl(url);
         log.info("url: {}", urlParams);
         String reponse = HttpClientUtil.doGet(urlParams);
@@ -90,9 +90,9 @@ public class MoguController {
     @ApiOperation(value = "", notes = "")
     public String recommend(@RequestBody QueryVo vo) {
         UrlParams urlParams = new UrlParams();
-        urlParams.setCookie("__mgjuuid=3e482381-6a92-4853-a140-76c06279731a; FRMS_FINGERPRINTN=_51veHI3TjmsSvMSjwKUVw; _ga=GA1.2.1520040884.1600612476; _mwp_h5_token_enc=2051c5bde0e1e5aa6669ab30bd9216ea; _mwp_h5_token=b0dbd2998c04c90da0a092d7763bd2fc_1601300153679");
-        urlParams.setReferer("https://h5.mogu.com/detail-normal/index.html?itemId=1mviwp6&acm=3.ms.1_1_1mviwp6.46.86084-68998.mLA6GscauEH7i.ct_1200-sd_117-swt_46-imt_7-c_2_17_589133381_556115615_1_0-t_mLA6GscauEH7i-lc_17-pit_2-c1_88933_nil_nil_1_556115615_50_0-dit_-idx_0-dm1_5001&cparam=MTYwMTU1NTI2MF8xamhpYjlhX2MwMjAxNjZlNzZhMzM4M2I1MDExNDY5ZDFmMjYzYTkzXzE3XzBfNTg5MTMzMzgxXzQ4OGZfMF8xXzBfNDk1XzFfMF9ycy04ODkzMy5ydC0xLnd0LTA=&ptp=32.rPCjb.undefined.1.5nLlcVtC");
-        String url = "https://api.mogu.com/h5/mwp.darwin.get/3/?data=%7B%22appPlat%22%3A%22m%22%2C%22plat%22%3A%22h5%22%2C%22pageSize%22%3A12%2C%22pid%22%3A%225018%22%2C%22iidE%22%3A%221mviwp6%22%7D&mw-appkey=100028&mw-ttid=NMMain%40mgj_h5_1.0&mw-t=1601558724575&mw-uuid=3e482381-6a92-4853-a140-76c06279731a&mw-h5-os=iOS&mw-sign=32fbd08d6449376eeebf58c4b8ffb255&callback=mwpCb3&_=1601558724577";
+        urlParams.setCookie("__mgjuuid=3e482381-6a92-4853-a140-76c06279731a; FRMS_FINGERPRINTN=_51veHI3TjmsSvMSjwKUVw; _ga=GA1.2.1520040884.1600612476; _mwp_h5_token_enc=eda2deb3fa51cc248bb4b3f4fdad0b56; _mwp_h5_token=564d4fa3cfcfdc97917c04767732e6db_1601905095482");
+        urlParams.setReferer("https://h5.mogu.com/detail-normal/index.html?itemId=1mjtcfo&acm=3.ms.1_1_1mjtcfo.46.86084-68998.e6MRRscyazKEA.ct_1200-sd_117-swt_46-imt_7-c_2_17_588337913_511007437_1_0-t_e6MRRscyazKEA-lc_17-pit_2-c1_88933_nil_nil_1_511007437_45_0-dit_-idx_0-dm1_5001&cparam=MTYwMTkwNTEwNl8xam15b2o0X2Y5YmExNWE1MWM5NzYxNDhlZTg2N2MzMDE1ZGE5ZTgyXzE3XzBfNTg4MzM3OTEzXzQ5OGRfMF8xXzBfNjYzXzFfMF9ycy04ODkzMy5ydC0xLnd0LTE=&ptp=32.rPCjb.undefined.1.LEjckPM4");
+        String url = "https://api.mogu.com/h5/mwp.darwin.get/3/?data=%7B%22appPlat%22%3A%22m%22%2C%22plat%22%3A%22h5%22%2C%22pageSize%22%3A12%2C%22pid%22%3A%225018%22%2C%22iidE%22%3A%221mjtcfo%22%7D&mw-appkey=100028&mw-ttid=NMMain%40mgj_h5_1.0&mw-t=1601905111532&mw-uuid=3e482381-6a92-4853-a140-76c06279731a&mw-h5-os=iOS&mw-sign=7dd964816e72dbd128f4df47e6d45fdc&callback=mwpCb3&_=1601905111539";
         urlParams.setUrl(url);
         log.info("url: {}", urlParams);
         String reponse = HttpClientUtil.doGet(urlParams);
